f9f1848014dd173e37c56c2f05458e889b95bd61
IanMJB <ianmjb@gmail.com>
2014-08-04 10:09:15 +0100

    file_names_to_csv.php takes a directory of NASA images and outputs a .csv of image-date and image-name.

---
+++ file_names_to_csv.php
@@ -0,0 +1,28 @@
+<?php
+
+/*********************************************************
+* Takes a directory full of NASA images - writes a CSV	 *
+* file containing pairs of image-date to image-name	 *
+* (image-date calculated from the file names, not meta-	 *
+* data.							 *
+* First argument is the directory of images.		 *
+* Second argument is the output file.			 *
+*********************************************************/
+
+$dir = $argv[1];
+$output = $argv[2];
+
+$files = scandir($dir);
+$results_file = fopen($output, 'w');
+foreach($files as $file)
+{
+	$file_name = $file;
+	$date = substr($file, 15, 10);
+	$date = strtotime($date);
+	$line = array(
+			$date,
+			$file_name
+		     );
+	fputcsv($results_file, $line);
+}
+fclose($results_file);
