606a7f8934b5f628d473746eb220fc7c3998e1d3
IanMJB <ianmjb@gmail.com>
2014-08-04 10:50:59 +0100

    Added end-date (based off of start-date of next file, if available, or timestamp estimate if not).

--- file_names_to_csv.php
+++ file_names_to_csv.php
@@ -2,9 +2,9 @@
 
 /*********************************************************
 * Takes a directory full of NASA images - writes a CSV	 *
-* file containing pairs of image-date to image-name	 *
-* (image-date calculated from the file names, not meta-	 *
-* data.							 *
+* file containing tuples of image-start-date,		 *
+* image-end-date and image-name (image-date calculated	 *
+* from the file names, not meta-data.			 *
 * First argument is the directory of images.		 *
 * Second argument is the output file.			 *
 *********************************************************/
@@ -12,16 +12,37 @@
 $dir = $argv[1];
 $output = $argv[2];
 
-$files = array_diff(scandir($dir), array('.', '..', '.DS_Store'));;
-$results_file = fopen($output, 'w');
+$lines = array();
+$files = array_diff(scandir($dir), array('.', '..', '.DS_Store'));
 foreach($files as $file)
 {
 	$file_name = $file;
-	$date = substr($file, 15, 10);
-	$date = strtotime($date);
+	$start_date = substr($file, 15, 10);
+	$start_date = strtotime($start_date);
 	$line = array(
-			$date,
+			$start_date,
 			$file_name
+		     );
+	$lines[] = $line;
+}
+
+$results_file = fopen($output, 'w');
+$i = 0;
+for($i = 0; $i < sizeof($lines); $i++)
+{
+	$line = $lines[$i];
+	if(isset($lines[$i+1]))
+	{
+		$end_date = $lines[$i+1][0] - 1;
+	}
+	else
+	{
+		$end_date = $line[0] + (86400 * 16) - 1;
+	}
+	$line = array(
+			$line[0],
+			$end_date,
+			$line[1]
 		     );
 	fputcsv($results_file, $line);
 }
