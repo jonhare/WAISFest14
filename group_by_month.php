8f2933b331e71a542bf4878c04708cf9ce99bf2f
IanMJB <ianmjb@gmail.com>
2014-08-04 14:50:00 +0100

    Small change to allow for non-overlapping output when using Plot2.

--- group_by_month.php
+++ group_by_month.php
@@ -4,6 +4,7 @@
 * Takes an sorted results file, counts the number of	 *
 * results from each month, and outputs them as a .csv	 *
 * file.							 *
+* Commented out sections are for non-overlapping output. *
 *********************************************************/
 
 $input_data = $argv[1];
@@ -15,7 +16,7 @@ for($y = 1970; $y <= 2012; $y++)
 {
 	for($m = 1; $m <= 12; $m++)
 	{
-		$month_year[$m.'-'.$y] = 0;
+		$month_year[str_pad($m, 2, '0', STR_PAD_LEFT).'-'.$y] = 0;
 	}
 }
 
@@ -29,8 +30,11 @@ while(($line = fgetcsv($input_data)) !== FALSE)
 fclose($input_data);
 
 $results_file = fopen($output, 'w');
+#$i = 1;
 foreach($month_year as $month => $count)
 {
 	fputcsv($results_file, array($month, $count), ',');
+	#fputcsv($results_file, array($i, $count), ',');
+	#$i++;
 }
 fclose($results_file);
